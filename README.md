d983e5a3f52387133a083835346188042844fcc0
jonathan gartland <jonny@hal.local>
2023-01-03 15:17:07 -0500

    add .idea dir to ignore

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mpn-js
-just a simple lookup tool for the MPN using the QT (this makes sense to the dozen people who might care)
+just a simple lookup tool for the MPN using the QT (this makes sense to the dozen people who might care)  
+will fill in more detail soon, but this is just the python project for this task in my repo that I reimplemented in javascript, the details are there for now
--- __tests__/mpn-lookup.test.js
+++ __tests__/mpn-lookup.test.js
@@ -1,30 +1,29 @@
 const QTArrays = require('../qtfile');
 const { describe, expect, test } = require('@jest/globals');
-// import {describe, expect, test} from '@jest/globals'
 
 describe('MPN Lookup Tests for QT values', () => {
-    test('lookup cases MPN', () => {
-        expect(QTArrays.getQTmpn(0))
-            .toStrictEqual(["<1.0", 0, 3.7]);
-        expect(QTArrays.getQTmpn(51))
-            .toStrictEqual([">200.5", 146.1, "infinite"]);
+  test('lookup cases MPN', () => {
+    expect(QTArrays.getQTmpn(0))
+      .toStrictEqual(["<1.0", 0, 3.7]);
+    expect(QTArrays.getQTmpn(51))
+      .toStrictEqual([">200.5", 146.1, "infinite"]);
 
-        expect(QTArrays.getQTmpn('x')).toBeFalsy();
-        expect(QTArrays.getQTmpn(-1)).toBeFalsy();
-        expect(QTArrays.getQTmpn(52)).toBeFalsy();
+    expect(QTArrays.getQTmpn('x')).toBeFalsy();
+    expect(QTArrays.getQTmpn(-1)).toBeFalsy();
+    expect(QTArrays.getQTmpn(52)).toBeFalsy();
 
-        for (let i = 0; i < 52; i++){
-            expect(QTArrays.getQTmpn(i)).toBeTruthy();
-        }
-    });
+    for (let i = 0; i < 52; i++){
+      expect(QTArrays.getQTmpn(i)).toBeTruthy();
+    }
+  });
 
-    test('lookup cases MPN QT2K', () => {
-        expect(QTArrays.getQT2Kmpn(49, 48))
-            .toStrictEqual([ '>2419.6', 1439.5, 'infinite' ]);
-    });
+  test('lookup cases MPN QT2K', () => {
+    expect(QTArrays.getQT2Kmpn(49, 48))
+      .toStrictEqual([ '>2419.6', 1439.5, 'infinite' ]);
+  });
 
-    test('lookup cases MPNLegio', () => {
-        expect(QTArrays.getQTLEGIOmpn(90, 6))
-            .toStrictEqual(">2272.6");
-    });
+  test('lookup cases MPNLegio', () => {
+    expect(QTArrays.getQTLEGIOmpn(90, 6))
+      .toStrictEqual(">2272.6");
+  });
 });
